fc00cdc160f94d7f99983d9969d2d814815f39bb
Malay Vaghasiya <144573490+malay811@users.noreply.github.com>
2024-03-03 16:58:25 +0530

    yes you are right

--- index.cpp
+++ index.cpp
@@ -6,5 +6,6 @@ int main()
     cout<<"Let's do something."<<endl;
     cout<<"No we can't do this."<<endl;
     cout<<"You are fooled"<<endl;
+    cout<<"We are going to do a great project."<<endl;
     return 0;
 }
