c93ebe5f96845fdf0d889403fa2794d06b2e5238
Malay Vaghasiya <144573490+malay811@users.noreply.github.com>
2024-03-03 16:26:09 +0530

    Trial code

---
+++ index.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Doing experiments."<<endl;
+    cout<<"Let's do something."<<endl;
+    cout<<"No we can't do this."<<endl;
+    
+    return 0;
+}
