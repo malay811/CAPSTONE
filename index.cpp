7c3ed3e7e39c689f83a5b12afca4097f0e6e2260
Malay Vaghasiya <144573490+malay811@users.noreply.github.com>
2024-03-03 16:38:14 +0530

    Added foolish

--- index.cpp
+++ index.cpp
@@ -5,6 +5,6 @@ int main()
     cout<<"Doing experiments."<<endl;
     cout<<"Let's do something."<<endl;
     cout<<"No we can't do this."<<endl;
-    
+    cout<<"You are fooled"<<endl;
     return 0;
 }
