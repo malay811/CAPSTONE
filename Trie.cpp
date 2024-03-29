a6c9d63f014c6e346a9a8fba21583a9d0de310ab
Jenish-Patel-65 <162017671+Jenish-Patel-65@users.noreply.github.com>
2024-03-30 00:34:38 +0530

    Suggestion function added in Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -29,10 +29,42 @@ class Trie {
             }
         delete node;
     }
+
  // destructor
  ~Trie() {
         deleteTrie(root);
     }
 
+
+//Function to find the suggestions for missplled word. 
+vector<string> Find_Suggestions(const string& word, const Trie& trie) const{
+
+    vector<string> suggestions;
+
+    // "Different approches to find suggestions."
+
+    // Insertion of all alphabets one by one at each position in input word. 
+    for (int i=0; i<=word.size(); i++){
+        for (char ch='a'; ch<='z'; ch++){
+            string new_Word = word.substr(0,i) + ch + word.substr(i);
+            if (trie.search(new_Word) || trie.search(convert_to_Lowercase(new_Word))){
+                suggestions.push_back(new_Word);
+            }
+        }
+    }
+
+    // Substitution of each alphabet in input word with all other alphabets.
+    for (int i=0; i<word.size(); i++){
+        for (char ch='a'; ch<='z'; ch++){
+            string new_Word = word.substr(0,i) + ch + word.substr(i+1);
+            if (ch != word[i]    &&     (trie.search(new_Word) || trie.search(convert_to_Lowercase(new_Word))) ){
+                suggestions.push_back(new_Word);
+            }
+        }
+    }
+
+
+    
+}  
 
 };
