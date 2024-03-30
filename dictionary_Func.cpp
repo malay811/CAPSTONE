21628ae4a26c47d30a495902806d7c1dec50eb7a
JAITRA SOLANKI <160627433+JaitraSolanki295@users.noreply.github.com>
2024-03-30 11:31:50 +0530

    Add writeTofile function

--- dictionary_Func.cpp
+++ dictionary_Func.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include<fstream>
+#include<string>
 using namespace std;
 
-//funcation that read words from file and insert into trie.
-void readWordsFromFileToTrie(const string &filename, Trie &dictionary)//Use const keyword for not changing file name and  this '&' for avoid duplication. 
+//function that read words from file and insert into trie.
+void readWordsFromFileToTrie(const string &filename, Trie &dictionary)//Use const keyword for not changing file name and  this '&' for avoid duplication.(Pass by Reference)
 {
     ifstream file(filename);
     // open the file with the name provided in the filename parameter for reading.
@@ -22,5 +23,22 @@ void readWordsFromFileToTrie(const string &filename, Trie &dictionary)//Use cons
     }
     else{
         cout << "Unable to open dictionary file: " << filename << ". Please ensure you have the necessary permissions to read the file." << endl;
+    }
+}
+
+
+//function to write content back into the file
+void WriteInToFile(const string &filename, const string &content)
+// This funcation take 2 parameter first is filename and second is the content writing to the filename's file
+{
+    ofstream file(filename);// This line create output file stream named file,and open filename's file to writing
+    
+    if(file.is_open())//check file is open or not
+    {
+        file<<content;//write content into the file
+        file.close();//close the file
+    }
+    else{
+        cout<<"Error to writing the content to file "<<filename <<endl; //error message for not opening file
     }
 }
