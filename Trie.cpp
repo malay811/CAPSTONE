29e49b59078295336230bd8bb421cb0d3b7b4ecc
Malay Vaghasiya <144573490+malay811@users.noreply.github.com>
2024-04-01 22:54:36 +0530

    Included insert and search functions

--- Trie.cpp
+++ Trie.cpp
@@ -35,7 +35,43 @@ class Trie {
         Trie() {
             root = new TrieNode('\0');
         }
-    
+
+    //inserting a word
+    void insert(string word)
+    {
+        TrieNode* node=root;
+        for(char ch : word)// It iterates over each character 'ch' in the input word.
+        {
+            int index=tolower(ch)-'a';
+            // to get an index in the range 0 to 25, representing the position of the character in the children array of the current node.
+            if(node->children[index]==nullptr)//charactor is not present
+            {
+                node->children[index]=new TrieNode(ch);//making a new node for the charactor
+            }
+            node=node->children[index];//element is present then updating the pointer
+        }
+        node->isWord=true;//making last charactor of the input string as a Terminal node
+    }
+
+    //searching a word
+    bool search(string word)
+    {
+        TrieNode* node=root;
+        for(char ch : word)
+        {
+            int index=tolower(ch)-'a';
+            if(node->children[index]==nullptr)//charactor is not present
+            {
+                return false;
+            }
+            node=node->children[index];//element is present
+        }
+        return node->isWord;//returning terminal node
+        //If node->isWord is true, it means that the entire word was found in the trie, and if it's false, 
+        //it means that the sequence of characters is part of a longer word but not a complete word itself.
+        //if trie have abcd word and input string is abc then it will return false.
+    }
+
     void deleteTrie(TrieNode* node) {
         if (node == nullptr) {
             return;
